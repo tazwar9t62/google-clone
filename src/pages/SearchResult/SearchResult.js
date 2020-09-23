d3a07242d6bc1b7ade9de78aa9efea5a1eb5c786
Tazwar <tazwar9t6@gmail.com>
2020-09-23 19:23:17 +0600

    added search options but got a bug

--- src/pages/SearchResult/SearchResult.js
+++ src/pages/SearchResult/SearchResult.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Searchbar from "../../components/Searchbar/Searchbar";
 import { useStateValue } from "../../StateProvider";
 import useGoogleSearch from "../../useGoogleSearch";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import "./SearchResult.css";
 
 const SearchResult = () => {
   const [{ term }, dispatch] = useStateValue();
@@ -8,7 +17,55 @@ const SearchResult = () => {
   // console.log(data);
   return (
     <div className="searchPage">
-      <div className="searchPage_header"></div>
+      <div className="searchPage_header">
+        <Link to="/">
+          <img
+            className="searchPage_header_image"
+            src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            alt="google logo"
+          />
+        </Link>
+        <div className="searchPage_header_body">
+          <Searchbar hiddenButtons />
+        </div>
+        
+        <div className="searchPage_option">
+          <div className="searchPage_option_left">
+            <div className="searchPage_single_option">
+              <SearchIcon />
+              <Link to="/all">All</Link>
+            </div>
+            <div className="searchPage_single_option">
+              <DescriptionIcon />
+              <Link to="/news">News</Link>
+            </div>
+            <div className="searchPage_single_option">
+              <ImageIcon />
+              <Link to="/images">Images</Link>
+            </div>
+            <div className="searchPage_single_option">
+              <LocalOfferIcon />
+              <Link to="/shopping">Shopping</Link>
+            </div>
+            <div className="searchPage_single_option">
+              <RoomIcon />
+              <Link to="/maps">maps</Link>
+            </div>
+            <div className="searchPage_single_option">
+              <MoreVertIcon />
+              <Link to="/more">more</Link>
+            </div>
+          </div>
+          <div className="searchPage_option_right">
+            <div className="searchPage_single_option">
+              <Link to="/setting">Setting</Link>
+            </div>
+            <div className="searchPage_single_option">
+              <Link to="/tools">Tools</Link>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="searchPage_Result">
         <h1>hlw {term} </h1>
       </div>
